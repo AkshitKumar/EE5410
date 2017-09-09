9bbfa2645fbdd3e6a2867bee94474ac33fa4a5a0
AkshitKumar <akshitkumar100@gmail.com>
2017-09-09 21:55:48 +0530

    initial commit

---
+++ nyquist_plot.m
@@ -0,0 +1,17 @@
+close all;
+clear;
+clc;
+
+w1 = 2 * pi * (0:0.01:1000-0.1) * 1i;
+w2 = 2 * pi * 1000 * exp(1i*(pi/2:-0.01:-pi/2));
+w3 = 2 * pi * (-1000+0.1 : 0.01 : -0.01)* 1i;
+
+W = [w1 w2 w3];
+
+eta = 0.01;
+
+for w0 = 100:100:1000
+    G = w0^2 ./ (W.*W + 2*eta*w0*W + w0^2);
+    plot(real(G),imag(G));
+    hold on;
+end  
